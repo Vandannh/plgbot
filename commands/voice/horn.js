439c0150c5c2995c41354d8bb8e4faf44bf34e29
Vandannh <daniel.hagg@live.se>
2020-02-16 22:00:29 +0100

    Added horn command

---
+++ commands/voice/horn.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: 'horn',
+    category: 'voice',
+    description: 'Plays airhorn.mp3 in a voice channel',
+    run: async(client, message, args) => {
+        if(message.member.voiceChannel) {
+            message.member.voiceChannel.join()
+                .then(connection => {
+                    const dispatcher = connection.playFile('../../files/airhorn.mp3');
+                    dispatcher.on('end', end => {
+                        message.member.voiceChannel.leave();
+                    });
+                });
+        } else {
+            message.channel.send('You need to be in a voice channel.')
+        }
+    }
+}
