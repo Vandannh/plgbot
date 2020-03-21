5791df44504fb8d17035cc99bf7c0c76ed5c155b
Vandannh <daniel.hagg@live.se>
2020-03-21 17:19:38 +0100

    Update watch.js

--- commands/info/watch.js
+++ commands/info/watch.js
@@ -5,6 +5,6 @@ module.exports = {
     category: 'info',
     description: 'Presents a link to a watch2gether.com room',
     run: async (client, message, args) => {
-        await message.channel.send("https://www.watch2gether.com/rooms/forfunz-wfwuj0h0rho6v60fgk");
+        await message.channel.send("https://www.watch2gether.com/rooms/epdjcb2pdxllhxlyaj");
     }
 }
